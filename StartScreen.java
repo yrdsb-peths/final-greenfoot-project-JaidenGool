74cf17dfc9ae34b7f607e3a9db1efe743e1c9956
Jaiden Gool <349162586@gapps.yrdsb.ca>
2024-05-22 13:38:41 -0400

    Create Title Screen

---
+++ StartScreen.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title Screen for the Cookie Clicker
+ * 
+ * @author Jaiden Gool 
+ * @version 05-22-24
+ */
+public class StartScreen extends World
+{
+    public StartScreen()
+    {    
+        super(900, 700, 1, false);
+    }
+}
