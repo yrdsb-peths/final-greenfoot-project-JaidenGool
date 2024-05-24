84aadafdb825b3e45aa33ffb435990482f54d8ba
Jaiden Gool <349162586@gapps.yrdsb.ca>
2024-05-24 16:21:32 -0400

    Add TitleScreen and GameScreen

---
+++ GameScreen.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Main game screen
+ * 
+ * @author Jaiden Gool
+ * @version 05-24-24
+ */
+public class GameScreen extends World
+{
+
+    public GameScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(900, 700, 1, false); 
+    }
+}
--- StartScreen.java
+++ StartScreen.java
@@ -8,8 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class StartScreen extends World
 {
+    //Title
+    Label titleLabel = new Label("Cookie Clicker", 75);
+    
+    //Instructions
+    Label instructionLabel = new Label("Press Enter to start", 65);
     public StartScreen()
     {    
         super(900, 700, 1, false);
+        
+        addObject(titleLabel, getWidth()/2, 260);
+        addObject(instructionLabel, getWidth()/2, 400);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("Enter"))
+        {
+            Greenfoot.setWorld(new GameScreen());
+        }
     }
 }
