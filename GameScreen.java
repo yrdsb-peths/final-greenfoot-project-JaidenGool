9e744d8393fd692c7ca636a73319791da46cd9e9
Jaiden Gool <349162586@gapps.yrdsb.ca>
2024-05-29 11:50:54 -0400

    Add Comments for GameScreen Class

--- GameScreen.java
+++ GameScreen.java
@@ -9,15 +9,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class GameScreen extends World
 {
     public GameScreen()
-    {    
+    {   
+        // Creates a 1440x900 screen
         super(1440, 900, 1, false);
         
+        // Border for upgrades and other interface objects
         Frame frame = new Frame();
         addObject(frame, 1000, 450);
         
+        // Clickable cookie
         Cookie cookie = new Cookie();
         addObject(cookie, getWidth()/4 -100, getHeight()/2);
         
+        // Label to display cookies owned
         ScoreDisplay scoreDisplay = new ScoreDisplay();
         addObject(scoreDisplay, getWidth()/4 -100, 200);
     }
